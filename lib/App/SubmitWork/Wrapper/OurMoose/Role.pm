84f6505a343588190774f29ae94a1d0012ded28e
Mark Fowler <mark@twoshortplanks.com>
2017-04-17 13:07:25 -0400

    add Ourperl, OurMoose, etc

---
+++ lib/App/SubmitWork/Wrapper/OurMoose/Role.pm
@@ -0,0 +1,27 @@
+package App::SubmitWork::Wrapper::OurMoose::Role;
+
+use App::SubmitWork::Wrapper::Ourperl;
+
+use Import::Into;
+use Moose::Exporter;
+use Moose::Role                    ();
+use MooseX::SemiAffordanceAccessor ();
+use namespace::autoclean           ();
+
+my ($import) = Moose::Exporter->setup_import_methods(
+    install => [ 'unimport', 'init_meta' ],
+    also    => ['Moose::Role'],
+);
+
+sub import {
+    my $for_role = caller();
+
+    $import->( undef, { into => $for_role } );
+    MooseX::SemiAffordanceAccessor->import( { into => $for_role } );
+
+    my $caller_level = 1;
+    App::SubmitWork::Wrapper::Ourperl->import::into($caller_level);
+    namespace::autoclean->import::into($caller_level);
+}
+
+1;
