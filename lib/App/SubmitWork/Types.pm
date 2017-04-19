5de5bc8ed3e96d51972a52df187d6637b795a0fe
Dave Rolsky <autarch@urth.org>
2017-04-19 13:11:49 -0500

    Load type libraries in Types.pm so that dzil sees them as deps

--- lib/App/SubmitWork/Types.pm
+++ lib/App/SubmitWork/Types.pm
@@ -6,6 +6,9 @@ our $VERSION = '1.000000';
 
 use parent 'MooseX::Types::Combine';
 
+use MooseX::Types::Common::Numeric ();
+use MooseX::Types::Moose           ();
+
 __PACKAGE__->provide_types_from(
     qw(
         MooseX::Types::Common::Numeric
